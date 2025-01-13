3e3b93d75f3959c7a414f7a07906e757b0a897cd
jeonghyeon00 <sjh00kr@naver.com>
2025-01-13 11:32:27 +0900

    Revert "feat: add kakao adfit"
    This reverts commit b919923a045c5587d436eb6e103093e408a9dd96.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,7 +13,6 @@ import * as config from 'lib/config';
 
 import styles from './styles.module.css';
 import HitCount from './HitCount';
-import KakaoAdFit from './KakaoAdFit';
 
 // TODO: merge the data and icons from PageSocial with the social links in Footer
 
@@ -37,7 +36,6 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <KakaoAdFit />
       <div className={styles.copyright}>
         Copyright {year} @ {config.author}
       </div>
@@ -130,7 +128,7 @@ export const FooterImpl: React.FC = () => {
           </a>
         )}
       </div>
-    </footer >
+    </footer>
   );
 };
 
